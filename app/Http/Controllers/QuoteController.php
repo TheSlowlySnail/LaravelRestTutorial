cc205e5888be5e619c514f7cef2c1a860f6b8e33
Helge Meiering <helge.meiering@gmail.com>
2018-04-11 09:26:04 +0200

    initial commit

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Quote;
+use Illuminate\Http\Request;
+
+class QuoteController extends Controller{
+
+    $quote = new Qutoe();
+
+    /**
+     * @param Request $request
+     */
+    public function postQuote(Request $request){
+
+    }
+
+    /**
+     *
+     */
+    public function getQuotes(){
+        
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     */
+    public function putQuote(Request $request, $id){
+        
+    }
+
+    /**
+     * @param $id
+     */
+    public function deleteQuote($id){
+        
+    }
+
+
+
+}
